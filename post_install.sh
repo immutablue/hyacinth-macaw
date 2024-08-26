e1f0e69401e13a98d5a08ba0ce447b62d8dd930c
Zach Podbielniak <z.podbielniak@disroot.org>
2024-08-26 01:59:11 -0400

    feat: add ptyxis settings

--- post_install.sh
+++ post_install.sh
@@ -122,6 +122,73 @@ install_artifacts_fonts() {
 }
 
 
+app_settings_ptyxis() {
+    local main_key="org.gnome.Ptyxis"
+    local profile_key="org.gnome.Ptyxis.Profile:/org/gnome/Ptyxis/Profiles"
+    local uuid=$(gsettings get "${main_key}" default-profile-uuid)
+
+    # If I have multiple profile uuids in the future.
+    #local uuid_arr=$(gsettings get "${main_key}" profile-uuids)
+    #local uuids="$(sed -En 's|[^0-9a-z]*||g; s|([0-9a-z]{32})|\1\n|gp' <<<${uuid_arr})"
+
+    # Main "global" settings
+    declare -A settings_kv=(
+        [audible-bell]="true"
+        [cursor-blink-mode]="system"
+        [cursor-shape]="block"
+        [default-columns]="uint32 80"
+        [default-rows]="uint32 24"
+        [enable-a11y]="false"
+        [font-name]="Hack Nerd Font Mono 10"
+        [interface-style]="system"
+        [new-tab-position]="last"
+        [restore-session]="true"
+        [restore-window-size]="true"
+        [scrollbar-policy]="system"
+        [text-blink-mode]="always"
+        [toast-on-copy-clipboard]="true"
+        [use-system-font]="false"
+        [visual-bell]="true"
+        [visual-process-leader]="true"
+        [window-size]="(uint32 223, uint32 65)"
+    )
+
+    # Per profile settings
+    declare -A uuid_kv=(
+        [backspace-binding]="ascii-delete"
+        [bold-is-bright]="true"
+        [cjk-ambiguous-width]="narrow"
+        [custom-command]=""
+        [default-container]="session"
+        [delete-binding]="delete-sequence"
+        [exit-action]="close"
+        [label]="My Profile"
+        [login-shell]="false"
+        [opacity]="1.0"
+        [palette]="Catppuccin Mocha"
+        [preserve-container]="always"
+        [preserve-directory]="safe"
+        [scroll-on-keystroke]="true"
+        [scroll-on-output]="false"
+        [scrollback-lines]="10000"
+        [use-custom-command]="false"
+        [use-proxy]="true"
+    )
+
+    for setting in ${!settings_kv[@]}
+    do 
+        gsettings set "${main_key}" "${setting}" "${settings_kv[$setting]}"
+    done
+
+    for setting in ${!uuid_kv[@]}
+    do 
+        settings set "${profile_key}/${uuid}/" "${setting}" "${settings_kv[$setting]}"
+    done
+    
+}
+
+
+
 prepare() {
     prepare_gitlab_ssh_keys
     artifacts_prework
@@ -134,7 +201,12 @@ install_all_artifacts() {
     install_artifacts_fonts
 }
 
+app_settings() {
+    app_settings_ptyxis
+}
+
 
 prepare
 install_all_artifacts
+app_settings
 
