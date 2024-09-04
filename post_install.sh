ceb0e616d2493ce980dc8c42eb033fde49097f71
Zach Podbielniak <z.podbielniak@disroot.org>
2024-09-03 23:55:07 -0400

    bug(install): brew path not set on initial install

--- post_install.sh
+++ post_install.sh
@@ -6,6 +6,8 @@ EXPECTED_INSTALL_DIR="/usr/immutablue-build-hyacinth-macaw"
 DOTFILES_GIT="git@gitlab.com:zachpodbielniak/dotfiles.git"
 THEME_GIT="https://github.com/vinceliuice/WhiteSur-gtk-theme.git"
 
+# Assume brew is not setup yet
+export PATH="$HOME/../linuxbrew/.linuxbrew/bin"
 
 prepare_gitlab_ssh_keys() {
     got_keys=$(grep -P "gitlab\.com" ~/.ssh/known_hosts)
