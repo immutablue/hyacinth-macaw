5cf4e4de8f03ed2524952dd51590e2d222af078e
Zach Podbielniak <z.podbielniak@disroot.org>
2025-01-12 21:37:52 -0500

    chore(workaround): overwrite post_install.sh for now

--- build/90-post.sh
+++ build/90-post.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 if [ -f "${CUSTOM_INSTALL_DIR}/build/99-common.sh" ]; then source "${CUSTOM_INSTALL_DIR}/build/99-common.sh"; fi
 if [ -f "./99-common.sh" ]; then source "./99-common.sh"; fi
 
+# Migrating to build scripts
+echo -e "#!/bin/bash\ntrue\n" > "${CUSTOM_INSTALL_DIR}/post_install.sh"
 
 # Build docs in case any were added with the image
 bash -c "cd /usr/immutablue/docs && hugo build"
